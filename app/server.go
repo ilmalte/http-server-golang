34f50db1a922125ecf0e83d8aced9cc98d80dd05
Daniele Maltese <17677294+ilmalte@users.noreply.github.com>
2024-07-18 21:44:18 +0200

    feat: Respond with 200

--- app/server.go
+++ app/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
@@ -11,17 +10,27 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", "127.0.0.1:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Failed to accept connection")
+			os.Exit(1)
+		}
+
+		fmt.Println("Accepted connection from ", conn.RemoteAddr())
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	}
+
+	// if err != nil {
+	// 	fmt.Println("Error accepting connection: ", err.Error())
+	// 	os.Exit(1)
+	// }
 }
