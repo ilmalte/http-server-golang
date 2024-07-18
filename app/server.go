f1bb19d212f3f6eb5fd372ade537355b83693f0d
Daniele Maltese <17677294+ilmalte@users.noreply.github.com>
2024-07-18 22:57:59 +0200

    feat: Extract URL path

--- app/server.go
+++ app/server.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "127.0.0.1:4221")
@@ -17,7 +17,6 @@ func main() {
 	}
 
 	for {
-
 		conn, err := l.Accept()
 
 		if err != nil {
@@ -26,11 +25,31 @@ func main() {
 		}
 
 		fmt.Println("Accepted connection from ", conn.RemoteAddr())
+		// read 4096 bytes at a time
+		buf := make([]byte, 4096)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Failed to read data from connection")
+			os.Exit(1)
+			continue
+		}
+		s := ""
+		if n > 0 {
+			l := string(buf[:n])
+			i := strings.Index(l, " HTTP")
+			if i != -1 {
+				s = l[:i]
+				i = strings.Index(l, "/")
+				s = s[i:]
+				fmt.Println("Request: ", s)
+			}
+		}
+
+		fmt.Println("Len: ", len(s))
+		if len(s) > 1 {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			continue
+		}
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	}
-
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
 }
