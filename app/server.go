cda77dde2d5a6b395d24f462423ddf95a61551e9
Maltese, Daniele <17677294+ilmalte@users.noreply.github.com>
2024-07-19 16:58:30 +0200

    feat: Return a file

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"net"
 	"os"
 	"strings"
@@ -52,19 +53,74 @@ func getResponse(request HTTPRequest) string {
 		response = fmt.Sprintf("%s\r\n\r\n", getStatus(405, "Method Not Allowed"))
 		return response
 	}
-
 	switch {
 	case request.Path == "/user-agent":
-		response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), len(request.UserAgent), request.UserAgent)
+		response = getResponseStr(request.UserAgent, "text/plain")
 	case strings.HasPrefix(request.Path, "/echo/"):
 		echo := strings.Split(string(request.Path), "/echo/")[1:][0]
-		response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), len(echo), echo)
+		response = getResponseStr(echo, "text/plain")
+	case strings.HasPrefix(request.Path, "/files/"):
+		fileContent := getFileContent(request.Path)
+		response = getResponseStr(fileContent, "application/octet-stream")
 	case request.Path != "/":
 		response = fmt.Sprintf("%s\r\n\r\n", getStatus(404, "Not Found"))
 	default:
 		response = fmt.Sprintf("%s\r\n\r\n", getStatus(200, "OK"))
 	}
 	return response
+}
+func getFileContent(path string) string {
+	err := error(nil)
+	directory := ""
+	file := fs.DirEntry(nil)
+	fileName := strings.Split(string(path), "/files/")[1:][0]
+
+	if len(os.Args) > 1 {
+		directory = os.Args[2]
+		if directory == "" {
+			fmt.Println("No directory specified")
+		}
+		file, err = getFile(directory, fileName)
+	}
+	if err != nil {
+		return fmt.Sprintf("%s\r\n\r\n", getStatus(500, "Internal Server Error"))
+	}
+	if file == nil {
+		return fmt.Sprintf("%s\r\n\r\n", getStatus(404, "Not Found"))
+	}
+	fileContent, err := os.ReadFile(directory + file.Name())
+	if err != nil {
+		return fmt.Sprintf("%s\r\n\r\n", getStatus(500, "Internal Server Error"))
+	}
+	return string(fileContent)
+}
+func getFile(directory string, fileName string) (fs.DirEntry, error) {
+	// Check if the specified directory exists
+	_, err := os.Stat(directory)
+	if err != nil {
+		fmt.Println("Directory does not exist")
+		return nil, err
+	}
+	// List files in the specified directory
+	files, err := os.ReadDir(directory)
+	if err != nil {
+		fmt.Println("Failed to read directory")
+		return nil, err
+	}
+	if len(files) == 0 {
+		fmt.Println("No files in the directory")
+		return nil, err
+	}
+	// Find file by name
+	for _, file := range files {
+		if file.Name() == fileName {
+			return file, nil
+		}
+	}
+	return nil, nil
+}
+func getResponseStr(s string, t string) string {
+	return fmt.Sprintf("%s\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), t, len(s), s)
 }
 func getStatus(code int, message string) string {
 	return fmt.Sprintf("HTTP/1.1 %d %s", code, message)
