3000e02dce051ca0a3930f9043b9e60d30cd6c8d
Maltese, Daniele <17677294+ilmalte@users.noreply.github.com>
2024-07-19 14:05:15 +0200

    feat: Concurrent connections

--- app/server.go
+++ app/server.go
@@ -30,35 +30,42 @@ func main() {
 			fmt.Println("Failed to accept connection")
 			os.Exit(1)
 		}
-
-		fmt.Println("Accepted connection from ", conn.RemoteAddr())
-		response := ""
-		req := make([]byte, 1024)
-		conn.Read(req)
-		request := getHttpRequest([]byte(req))
-		if request.Method != "GET" {
-			response = getStatus(405, "Method Not Allowed")
-			conn.Write([]byte(response))
-			conn.Close()
-		}
-
-		switch {
-		case request.Path == "/user-agent":
-			response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), len(request.UserAgent), request.UserAgent)
-		case strings.HasPrefix(request.Path, "/echo/"):
-			echo := strings.Split(string(request.Path), "/echo/")[1:][0]
-			response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), len(echo), echo)
-		case request.Path != "/":
-			response = getStatus(404, "Not Found")
-		default:
-			response = getStatus(200, "OK")
-		}
-		fmt.Println("Response ", string([]byte(response)))
-		conn.Write([]byte(response))
-		conn.Close()
+		go handleConnection(conn)
 	}
 }
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	req := make([]byte, 1024)
+	_, err := conn.Read(req)
+	if err != nil {
+		fmt.Println("Failed to read request")
+		return
+	}
+	httpReq := getHttpRequest(req)
+	fmt.Println(httpReq)
+	response := getResponse(httpReq)
+	conn.Write([]byte(response))
+}
+func getResponse(request HTTPRequest) string {
+	response := ""
+	if request.Method != "GET" {
+		response = getStatus(405, "Method Not Allowed")
+		return response
+	}
 
+	switch {
+	case request.Path == "/user-agent":
+		response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), len(request.UserAgent), request.UserAgent)
+	case strings.HasPrefix(request.Path, "/echo/"):
+		echo := strings.Split(string(request.Path), "/echo/")[1:][0]
+		response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), len(echo), echo)
+	case request.Path != "/":
+		response = getStatus(404, "Not Found")
+	default:
+		response = getStatus(200, "OK")
+	}
+	return response
+}
 func getStatus(code int, message string) string {
 	return fmt.Sprintf("HTTP/1.1 %d %s", code, message)
 }
