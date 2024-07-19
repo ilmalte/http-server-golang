caae8c6306889b61fd9adb62643f9d940ff045fc
Maltese, Daniele <17677294+ilmalte@users.noreply.github.com>
2024-07-19 13:40:13 +0200

    refactor: Read header

--- app/server.go
+++ app/server.go
@@ -4,9 +4,16 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 )
+
+type HTTPRequest struct {
+	Method    string
+	Path      string
+	Headers   map[string]string
+	Body      string
+	UserAgent string
+}
 
 func main() {
 	fmt.Println("Program has started!")
@@ -19,35 +26,62 @@ func main() {
 
 	for {
 		conn, err := l.Accept()
-
 		if err != nil {
 			fmt.Println("Failed to accept connection")
 			os.Exit(1)
 		}
 
 		fmt.Println("Accepted connection from ", conn.RemoteAddr())
-
+		response := ""
 		req := make([]byte, 1024)
 		conn.Read(req)
-		switch {
-		case strings.HasPrefix(string(req), "GET /user-agent"):
-			res := strings.Split(string(req), "User-Agent: ")[1:]
-			res = strings.Split(string(res[0]), "\r\n\r\n")[:1]
-			u := res[0]
-			conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(u)) + "\r\n\r\n" + u))
-			conn.Close()
-		case strings.HasPrefix(string(req), "GET /echo/"):
-			res := strings.Split(string(req), "/echo/")[1:]
-			res = strings.Split(res[0], " HTTP/1.1")
-			echo := res[0]
-			conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(echo)) + "\r\n\r\n" + echo))
-			conn.Close()
-		case !strings.HasPrefix(string(req), "GET / HTTP/1.1"):
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			conn.Close()
-		default:
-			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		request := getHttpRequest([]byte(req))
+		if request.Method != "GET" {
+			response = getStatus(405, "Method Not Allowed")
+			conn.Write([]byte(response))
 			conn.Close()
 		}
+
+		switch {
+		case request.Path == "/user-agent":
+			response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), len(request.UserAgent), request.UserAgent)
+		case strings.HasPrefix(request.Path, "/echo/"):
+			echo := strings.Split(string(request.Path), "/echo/")[1:][0]
+			response = fmt.Sprintf("%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", getStatus(200, "OK"), len(echo), echo)
+		case request.Path != "/":
+			response = getStatus(404, "Not Found")
+		default:
+			response = getStatus(200, "OK")
+		}
+		fmt.Println("Response ", string([]byte(response)))
+		conn.Write([]byte(response))
+		conn.Close()
+	}
+}
+
+func getStatus(code int, message string) string {
+	return fmt.Sprintf("HTTP/1.1 %d %s", code, message)
+}
+func getHttpRequest(req []byte) HTTPRequest {
+	reqStr := string(req)
+	reqLines := strings.Split(reqStr, "\r\n")
+	reqLine := strings.Split(reqLines[0], " ")
+	method := reqLine[0]
+	path := reqLine[1]
+	headers := make(map[string]string)
+	for _, line := range reqLines[1:] {
+		if line == "" {
+			break
+		}
+		header := strings.Split(line, ": ")
+		headers[header[0]] = header[1]
+	}
+	body := reqLines[len(reqLines)-1]
+	return HTTPRequest{
+		Method:    method,
+		Path:      path,
+		Headers:   headers,
+		Body:      body,
+		UserAgent: headers["User-Agent"],
 	}
 }
