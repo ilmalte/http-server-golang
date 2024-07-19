d7b1a2a086e4a758023050bc361a645bca913f3a
Maltese, Daniele <17677294+ilmalte@users.noreply.github.com>
2024-07-19 11:40:18 +0200

    feat: Read header

--- app/server.go
+++ app/server.go
@@ -29,18 +29,25 @@ func main() {
 
 		req := make([]byte, 1024)
 		conn.Read(req)
-		if strings.HasPrefix(string(req), "GET /echo/") {
+		switch {
+		case strings.HasPrefix(string(req), "GET /user-agent"):
+			res := strings.Split(string(req), "User-Agent: ")[1:]
+			res = strings.Split(string(res[0]), "\r\n\r\n")[:1]
+			u := res[0]
+			conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(u)) + "\r\n\r\n" + u))
+			conn.Close()
+		case strings.HasPrefix(string(req), "GET /echo/"):
 			res := strings.Split(string(req), "/echo/")[1:]
 			res = strings.Split(res[0], " HTTP/1.1")
 			echo := res[0]
 			conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + strconv.Itoa(len(echo)) + "\r\n\r\n" + echo))
 			conn.Close()
-			return
-		} else if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+		case !strings.HasPrefix(string(req), "GET / HTTP/1.1"):
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 			conn.Close()
-			return
+		default:
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+			conn.Close()
 		}
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	}
 }
